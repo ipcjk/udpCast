43c93795cffb7b297ce73140820ec34cda21d5f4
joerg kost <jk@ip-clear.de>
2019-06-17 13:47:01 +0200

    Initial ;)

--- udpCast.go
+++ udpCast.go
@@ -7,7 +7,23 @@ import (
 	"net"
 )
 
+type transmit struct {
+	data [1518]byte
+	len  int
+}
+
+type udpRecv struct {
+	daddr   *net.UDPAddr
+	dconn   *net.UDPConn
+	network string
+	queue   chan transmit
+}
+
 func main() {
+	/* some local variables */
+	var err error
+	reader := make(chan transmit)
+
 	/* listener */
 	lAddr := flag.String("ia", "", "listening address")
 	lPort := flag.Int("ip", 2009, "listening port")
@@ -19,13 +35,7 @@ func main() {
 	/* parse arguments */
 	flag.Parse()
 
-	/* destination endpoint */
-	daddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", *oAddr, *oPort))
-	if err != nil {
-		log.Fatalf("cant resolve destination endpoint: %s", err)
-	}
-
-	/* server endpoint */
+	/* server listener */
 	sAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", *lAddr, *lPort))
 	if err != nil {
 		log.Fatalf("cant resolve endpoint: %s", err)
@@ -37,32 +47,55 @@ func main() {
 		log.Fatalf("cant listen endpoint: %s", err)
 	}
 
+	/* client with queue and endpoint */
+	udp := udpRecv{network: fmt.Sprintf("%s:%d", *oAddr, *oPort)}
+	udp.queue = make(chan transmit, 100)
+	udp.daddr, err = net.ResolveUDPAddr("udp", udp.network)
+	if err != nil {
+		log.Fatalf("cant resolve destination endpoint: %s", err)
+	}
+
 	/* pseudo dial output for outgoing connection */
-	dConn, err := net.DialUDP("udp", nil, daddr)
+	udp.dconn, err = net.DialUDP("udp", nil, udp.daddr)
 	if err != nil {
 		log.Fatalf("cant build udp socket to server: %s", err)
 	}
 
+	go func(client udpRecv) {
+		for {
+			select {
+			case toTransmit := <-client.queue:
+				log.Print("Packet angekommen")
+				udp.dconn.Write(toTransmit.data[0:toTransmit.len])
+			}
+		}
+	}(udp)
+
+	/* reader goroutine is pushing
+	duplicating data to waiting outgoing threads */
+	go func() {
+		for {
+			select {
+			case toTransmit := <-reader:
+				{
+					udp.queue <- toTransmit
+				}
+			}
+		}
+
+	}()
+
 	func(s *net.UDPConn, d *net.UDPAddr) {
 		buffer := [1518]byte{}
-
 		for {
-
 			/* read from source */
 			n, err := s.Read(buffer[0:])
 			if err != nil {
 				log.Println(err)
 			}
-
-			/* relay to destination */
-			_, err = dConn.Write(buffer[0:n])
-			if err != nil {
-				log.Println(err)
-			}
-
+			/* Write to channel */
+			reader <- transmit{data: buffer, len: n}
 		}
-	}(s, daddr)
-
-	fmt.Println("End of the line")
+	}(s, udp.daddr)
 
 }
