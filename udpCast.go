8c73cf95cdef57260f3854a9e823852594517cf2
joerg kost <jk@ip-clear.de>
2019-06-17 14:44:20 +0200

    Prototype of multiple destinations

--- udpCast.go
+++ udpCast.go
@@ -5,37 +5,114 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 )
 
+/* structure we use for copying the incoming stream */
 type transmit struct {
 	data [1518]byte
 	len  int
 }
 
-type udpRecv struct {
+/* simple destination type for looping */
+type destinations struct {
+	host string
+	port int
+}
+
+/* structure of each udp destination*/
+type udpDest struct {
 	daddr   *net.UDPAddr
 	dconn   *net.UDPConn
 	network string
 	queue   chan transmit
 }
 
+/* read data and transmit over the UDP socket */
+func (u *udpDest) poller() {
+	for {
+		select {
+		case toTransmit := <-u.queue:
+			u.dconn.Write(toTransmit.data[0:toTransmit.len])
+		}
+	}
+}
+
+/* newUdpDest creates new initialized udp destination structure */
+func newUdpDest(oAddr string, oPort int) udpDest {
+	var err error
+
+	/* client with queue and endpoint */
+	udp := udpDest{network: fmt.Sprintf("%s:%d", oAddr, oPort)}
+	udp.daddr, err = net.ResolveUDPAddr("udp", udp.network)
+
+	/* check for error condition */
+	if err != nil {
+		log.Fatalf("cant resolve destination endpoint: %s", err)
+	}
+
+	/* pseudo dial output for outgoing connection */
+	udp.dconn, err = net.DialUDP("udp", nil, udp.daddr)
+
+	/* check for error condition */
+	if err != nil {
+		log.Fatalf("cant build udp socket to server: %s", err)
+	}
+
+	/* new buffered channel for receiving data */
+	udp.queue = make(chan transmit, 1000)
+
+	/* return our freshly created udp object */
+	return udp
+
+}
+
+/* loveley main function */
 func main() {
 	/* some local variables */
 	var err error
 	reader := make(chan transmit)
 
+	/* create new slices of receiver channels, maximum 10 */
+	set := make([]chan transmit, 0)
+
 	/* listener */
 	lAddr := flag.String("ia", "", "listening address")
 	lPort := flag.Int("ip", 2009, "listening port")
-
-	/* destination */
-	oAddr := flag.String("oa", "127.0.0.1", "destination server")
-	oPort := flag.Int("op", 2010, "destination port")
+	sDestinations := flag.String("dest", "127.0.0.1:2010,127.0.0.1:2011", "destinations, comma seperated")
 
 	/* parse arguments */
 	flag.Parse()
 
-	/* server listener */
+	/* split input into clients */
+	clients := strings.Split(*sDestinations, ",")
+
+	/* parse the input slice of clients and create new objects */
+	for i := range clients {
+
+		/* create  and startup new destinations */
+		singleClient := strings.Split(clients[i], ":")
+
+		port, err := strconv.Atoi(singleClient[1])
+		if err != nil {
+			log.Fatalf("Cant parse client %s", singleClient)
+		}
+
+		udp := newUdpDest(singleClient[0], port)
+
+		/* put the receivement channel of the newly created udp object into the slice */
+		set = append(set, udp.queue)
+
+		/* start listener thread */
+		go func() {
+			fmt.Println("Created destination ", udp.network)
+			udp.poller()
+		}()
+
+	}
+
+	/* create server listener */
 	sAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", *lAddr, *lPort))
 	if err != nil {
 		log.Fatalf("cant resolve endpoint: %s", err)
@@ -47,45 +124,24 @@ func main() {
 		log.Fatalf("cant listen endpoint: %s", err)
 	}
 
-	/* client with queue and endpoint */
-	udp := udpRecv{network: fmt.Sprintf("%s:%d", *oAddr, *oPort)}
-	udp.queue = make(chan transmit, 100)
-	udp.daddr, err = net.ResolveUDPAddr("udp", udp.network)
-	if err != nil {
-		log.Fatalf("cant resolve destination endpoint: %s", err)
-	}
-
-	/* pseudo dial output for outgoing connection */
-	udp.dconn, err = net.DialUDP("udp", nil, udp.daddr)
-	if err != nil {
-		log.Fatalf("cant build udp socket to server: %s", err)
-	}
-
-	go func(client udpRecv) {
-		for {
-			select {
-			case toTransmit := <-client.queue:
-				log.Print("Packet angekommen")
-				udp.dconn.Write(toTransmit.data[0:toTransmit.len])
-			}
-		}
-	}(udp)
-
 	/* reader goroutine is pushing
-	duplicating data to waiting outgoing threads */
-	go func() {
+	duplicating data to the waiting, udp-objects */
+	go func(set []chan transmit) {
 		for {
 			select {
 			case toTransmit := <-reader:
 				{
-					udp.queue <- toTransmit
+					go func(toTransmit transmit) {
+						for _, v := range set {
+							v <- toTransmit
+						}
+					}(toTransmit)
 				}
 			}
 		}
+	}(set)
 
-	}()
-
-	func(s *net.UDPConn, d *net.UDPAddr) {
+	func(s *net.UDPConn) {
 		buffer := [1518]byte{}
 		for {
 			/* read from source */
@@ -96,6 +152,6 @@ func main() {
 			/* Write to channel */
 			reader <- transmit{data: buffer, len: n}
 		}
-	}(s, udp.daddr)
+	}(s)
 
 }
